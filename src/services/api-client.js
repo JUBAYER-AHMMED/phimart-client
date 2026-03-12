e63d4060952eb55903434bdbb49907b48b05985c
JUBAYER AHMMED <jubayerahmmed105@gmail.com>
2026-03-12 16:41:32 +0600

    set the base url to the backend

--- src/services/api-client.js
+++ src/services/api-client.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const apiClient = axios.create({
   // baseURL: "https://phimart.vercel.app/api/v1",
-  // baseURL: "https://phimart-snowy.vercel.app/api/v1",
-  baseURL: "http://127.0.0.1:8000/api/v1",
+  baseURL: "https://phimart-snowy.vercel.app/api/v1",
+  // baseURL: "http://127.0.0.1:8000/api/v1",
 });
 
 export default apiClient;
